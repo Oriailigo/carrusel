af70f79dd8faaf433ed0a44d32023661b3a68e19
ori <ori_larusa@hotmail.com>
2022-01-12 17:40:09 -0300

    funcionan los controles, falta la accesibilidad

--- js/main.js
+++ js/main.js
@@ -1,24 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const elementosCarousel = document.querySelectorAll('.carousel');
+document.addEventListener('DOMContentLoaded', function() {
+    M.AutoInit();
 
-    const objecto = M.Carousel.init(elementosCarousel, {
+   
+
+    //carousel activation
+    var elems = document.querySelectorAll('.carousel');
+    var xinstances = M.Carousel.init(elems, 
+    {
         duration: 150,
-        dist: -80,
-        shift: 5,
-        padding: 5,
-        numVisible: 3,
-        indicators: false,
-        noWrap: false
-        // fullWidth: true
+                dist: -80,
+                shift: 5,
+                padding: 5,
+                numVisible: 3,
+                indicators: false,
+                noWrap: false
     });
 
-    var instance = M.Carousel.getInstance(elementosCarousel);
-    const botonNext = document.getElementById('button-next');
-    botonNext.onclick = () => instance.next(2);
-    // botonNext.addEventListener("onclick",()=>{
-            
-    // })
-    
-});
-
-// const elementos = document.querySelectorAll('.carousel').addEventListener("click");
+    //carousel Next function
+    window.next = function() {
+      var el = document.querySelector(".carousel");
+      var l = M.Carousel.getInstance(el);
+      l.next(1);
+    }
+    //carousel previous function
+    window.prev = function() {
+        var el = document.querySelector(".carousel");
+        var l = M.Carousel.getInstance(el);
+        l.prev(1);
+    }
+})
